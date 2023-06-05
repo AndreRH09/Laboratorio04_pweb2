fe3cbb71e4648edac4a9dfcf3e64d91c7fb9f501
AndreRH09 <aanazcoh@unsa.edu.pe>
2023-06-05 00:28:44 -0500

    ejercicio 2f

--- Ejercicio2f.py
+++ Ejercicio2f.py
@@ -2,8 +2,29 @@ from interpreter import draw
 from chessPictures import *
 
 
-image = rock.setBackground(square)
 
+image = square
+for i in range(7):
+    if (i%2 ==0):
+        image = image.join(square.negative())
+    else:
+        image = image.join(square)
+
+
+line = square 
+for i in range(7):
+    if (i%2 ==0):
+        line = line.join(square.negative())
+    else:
+        line = line.join(square)
+
+
+
+for a in range(3):
+    if (a%2 ==0):
+        image = image.up(line.horizontalMirror())
+    else:
+        image = image.up(line)
 draw(image)
 
 
